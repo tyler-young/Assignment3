9a8c9729e7eb0a11db9dd55aec84d8f30c8d3495
Tyler Young <tyler.young@mail.mcgill.ca>
2017-02-03 12:56:30 -0500

    Change

--- src/ca/mcgill/ecse202/a3/Question1.java
+++ src/ca/mcgill/ecse202/a3/Question1.java
@@ -4,8 +4,12 @@ public class Question1 {
 
   public static void main(String[] args) {
 
+<<<<<<< HEAD
     System.out.println("Tyler Young");
     System.out.println("Young Tyler Forever");
+=======
+    System.out.println("Hellow World");
+>>>>>>> parent of ccd8ace... Changed print statement output
   }
 
 }
