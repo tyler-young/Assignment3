80b9fd6b06f8eb3be6f25df5da54f45407c134ae
Tyler Young <tyler.young@mail.mcgill.ca>
2017-02-03 12:53:02 -0500

    Added print statement

--- src/ca/mcgill/ecse202/a3/Question1.java
+++ src/ca/mcgill/ecse202/a3/Question1.java
@@ -3,8 +3,8 @@ package ca.mcgill.ecse202.a3;
 public class Question1 {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
 
+    System.out.println("Hellow World");
   }
 
 }
